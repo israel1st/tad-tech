c3b35ee7db25752bc6a9bb858483d8fbed3959e8
israel1st <israelbassey.ib@gmail.com>
2024-10-17 13:33:06 +0100

    Add ESLint to client directory

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -8,14 +8,15 @@ export default [
   },
   {
     languageOptions: {
-      globals: globals.browser,
+      globals: { ...globals.browser, ...globals.node }, // Supports both browser and Node.js globals
     },
   },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
     rules: {
-      "no-unused-vars": "warn", // Change to "off" to ignore unused vars
+      "no-unused-vars": "warn", // Change to "off" to completely ignore unused vars warnings
+      "no-undef": "off",        // Turn off the 'no-undef' rule if you don't want undefined variable warnings
       // Add other rules as needed
     },
   },
